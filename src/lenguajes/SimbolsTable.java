857a155c1a23c72f8ba891fa25c7cefc045a0f97
juanka588 <jcrodriguezd@unal.edu.co>
2016-08-29 22:04:38 -0500

    fix editor

--- src/lenguajes/SimbolsTable.java
+++ src/lenguajes/SimbolsTable.java
@@ -14,15 +14,43 @@ import java.util.Map;
  */
 public class SimbolsTable {
 
+    public static final Map<String, Integer> RESERVED_WORDS;
     public static final Map<String, Integer> SIMBOLS_MAP;
 
     static {
+        RESERVED_WORDS = new HashMap<>();
+        RESERVED_WORDS.put("id", Token.ID);
+        RESERVED_WORDS.put("proceso", Token.PROCESS);
+        RESERVED_WORDS.put("entero", Token.INTEGER);
+        RESERVED_WORDS.put("definir", Token.DEFINITION);
+        RESERVED_WORDS.put("real", Token.REAL);
+        RESERVED_WORDS.put("finproceso", Token.END_PROCESS);
+    }
+
+    static {
         SIMBOLS_MAP = new HashMap<>();
-        SIMBOLS_MAP.put("id", Token.ID);
-        SIMBOLS_MAP.put("proceso", Token.PROCESS);
-        SIMBOLS_MAP.put("entero", Token.INTEGER);
-        SIMBOLS_MAP.put("definir", Token.DEFINITION);
-        SIMBOLS_MAP.put("real", Token.REAL);
-        SIMBOLS_MAP.put("finproceso", Token.END_PROCESS);
+        SIMBOLS_MAP.put("~", Token.ID);
+        SIMBOLS_MAP.put("=", Token.PROCESS);
+        SIMBOLS_MAP.put("<-", Token.INTEGER);
+        SIMBOLS_MAP.put("<>", Token.DEFINITION);
+        SIMBOLS_MAP.put("<", Token.REAL);
+        SIMBOLS_MAP.put(">", Token.END_PROCESS);
+        SIMBOLS_MAP.put("<=", Token.END_PROCESS);
+        SIMBOLS_MAP.put(">=", Token.END_PROCESS);
+        SIMBOLS_MAP.put("+", Token.END_PROCESS);
+        SIMBOLS_MAP.put("-", Token.END_PROCESS);
+        SIMBOLS_MAP.put("/", Token.END_PROCESS);
+        SIMBOLS_MAP.put("*", Token.END_PROCESS);
+        SIMBOLS_MAP.put("%", Token.END_PROCESS);
+        SIMBOLS_MAP.put(";", Token.END_PROCESS);
+        SIMBOLS_MAP.put(":", Token.END_PROCESS);
+        SIMBOLS_MAP.put("(", Token.END_PROCESS);
+        SIMBOLS_MAP.put(")", Token.END_PROCESS);
+        SIMBOLS_MAP.put("[", Token.END_PROCESS);
+        SIMBOLS_MAP.put("]", Token.END_PROCESS);
+        SIMBOLS_MAP.put("|", Token.END_PROCESS);
+        SIMBOLS_MAP.put("&", Token.END_PROCESS);
+        SIMBOLS_MAP.put(",", Token.END_PROCESS);
+        SIMBOLS_MAP.put("^", Token.END_PROCESS);
     }
 }
