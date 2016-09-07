ba6fce64610f52e80085228e77e3849db1099362
Estudiante <Estudiante@FI454402-17.unal.loc>
2016-09-07 12:23:49 -0500

    Merge origin/master

--- test/lenguajes/LexicalAnalyserTest.java
+++ test/lenguajes/LexicalAnalyserTest.java
@@ -6,6 +6,10 @@
 package lenguajes;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static lenguajes.LexicalAnalyser.main2;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -16,14 +20,14 @@ import static org.junit.Assert.*;
  * @author JuanCamilo
  */
 public class LexicalAnalyserTest {
-    
+
     public LexicalAnalyserTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
@@ -34,11 +38,18 @@ public class LexicalAnalyserTest {
     @org.junit.Test
     public void testMain() throws Exception {
         System.out.println("main");
-        File f=new File(".");
-        File[] files = f.listFiles();
+        System.out.println("main");
+        File f = new File("./ejemplos/");
+        File[] files = f.listFiles((File dir, String name) -> name.endsWith(".in"));
         for (File file : files) {
-            LexicalAnalyser.main(file.getName());
+            try {
+                main2(new String[]{file.getAbsolutePath()});
+            } catch (IOException ex) {
+                Logger.getLogger(LexicalAnalyser.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (LexicalException ex) {
+                Logger.getLogger(LexicalAnalyser.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
-    
+
 }
