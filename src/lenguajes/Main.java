cb1b591c8200c8836fd8da46fd2dd6fed1209414
juanka588 <jcrodriguezd@unal.edu.co>
2016-09-25 13:26:54 -0500

    fix unary and function calls

--- src/lenguajes/Main.java
+++ src/lenguajes/Main.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException, FileNotFoundException, LexicalException {
-        SyntacticAnalyser sa = new SyntacticAnalyser("grammar.txt","test11.txt");
+        SyntacticAnalyser sa = new SyntacticAnalyser("grammar.txt","test17.txt");
         sa.setToken(LexicalAnalyser.token);
         sa.getInitRule().execute();
         System.out.println("El analisis sintactico ha finalizado correctamente.");
