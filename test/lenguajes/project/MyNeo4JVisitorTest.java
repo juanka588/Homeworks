d69a636edb17e53e1f44d632cf8a7798151301fb
AlejandroGiraldoLeon <luis@uplay.com.br>
2016-11-14 16:36:03 -0500

    test 3.2

--- test/lenguajes/project/MyNeo4JVisitorTest.java
+++ test/lenguajes/project/MyNeo4JVisitorTest.java
@@ -45,10 +45,23 @@ public class MyNeo4JVisitorTest {
         ParseTree tree = parser.init();
         MyNeo4JVisitor<Object> loader = new MyNeo4JVisitor<>();
         String translation = (String) loader.visit(tree);
-        assertEquals("CREATE TABLE IF NOT EXISTS Person (person_id LONG NOT NULL AUTO_INCREMENT,name VARCHAR(255),age FLOAT(64), PRIMARY KEY (person_id));INSERT INTO Person VALUES (\"juan\",23);"
+        assertEquals("CREATE TABLE IF NOT EXISTS Person (person_id LONG NOT NULL AUTO_INCREMENT,age FLOAT(64),name VARCHAR(255), PRIMARY KEY (person_id));INSERT INTO Person (age,name) VALUES (23,\"juan\");"
                 , translation);
     }
 
+    @Test
+    public void testCreate11() throws FileNotFoundException, IOException {
+        ANTLRInputStream input = new ANTLRInputStream("create (h:Person)");
+        Neo4JLexer lexer = new Neo4JLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Neo4JParser parser = new Neo4JParser(tokens);
+        ParseTree tree = parser.init();
+        MyNeo4JVisitor<Object> loader = new MyNeo4JVisitor<>();
+        String translation = (String) loader.visit(tree);
+        assertEquals("CREATE TABLE IF NOT EXISTS Person (person_id LONG NOT NULL AUTO_INCREMENT,age FLOAT(64),name VARCHAR(255), PRIMARY KEY (person_id));INSERT INTO Person (age,name) VALUES (23,\"juan\");"
+                , translation);
+    }
+    
     @Test
     public void testCreate2() throws FileNotFoundException, IOException {
         ANTLRInputStream input = new ANTLRInputStream("create (h:Person{name:\"juan\",age:23})"
@@ -61,6 +74,7 @@ public class MyNeo4JVisitorTest {
         String translation = (String) loader.visit(tree);
         assertEquals("CREATE TABLE Person (name varchar());", translation);
     }
+    
     @Test
     public void testCreate3() throws FileNotFoundException, IOException {
         ANTLRInputStream input = new ANTLRInputStream("create (h:Person{name:12345})-[:EMPLOYEE]->(h:Person{name:123})");
@@ -71,10 +85,27 @@ public class MyNeo4JVisitorTest {
         MyNeo4JVisitor<Object> loader = new MyNeo4JVisitor<>();
         String translation = (String) loader.visit(tree);
         
-        assertEquals("CREATE TABLE IF NOT EXISTS Person_Person (person_person_id LONG NOT NULL AUTO_INCREMENT,destination LONG, FOREIGN KEY (destination) REFERENCES Person(person_id),label LONG, FOREIGN KEY (label) REFERENCES Label(label_id),origin LONG, FOREIGN KEY (origin) REFERENCES Person(Person_id), PRIMARY KEY (person_person_id));CREATE TABLE IF NOT EXISTS label (label_id LONG AUTOINCREMENT, label_name VARCHAR(255) UNIQUE, PRIMARY KEY (label_id));"
-                + "SET @label := (select label.label_id from label where label.label_name=\"EMPLOYEE\" limit 1);"
-                + "SET @origin :=(select Person.Person_id from Person where Person.name=12345 limit 1) ;"
-                + "SET @destination :=(select Person.Person_id from Person where Person.name=12345 limit 1) ;"
+        assertEquals("CREATE TABLE IF NOT EXISTS Person_Person (person_person_id LONG NOT NULL AUTO_INCREMENT,destination LONG, FOREIGN KEY (destination) REFERENCES Person(person_id),label LONG, FOREIGN KEY (label) REFERENCES Label(label_id),origin LONG, FOREIGN KEY (origin) REFERENCES Person(person_id), PRIMARY KEY (person_person_id));CREATE TABLE IF NOT EXISTS Label (label_id LONG AUTOINCREMENT, label_name VARCHAR(255) UNIQUE, PRIMARY KEY (label_id));"
+                + "SET @label := (select Label.label_id from Label where Label.label_name=\"EMPLOYEE\" limit 1);"
+                + "SET @origin :=(select Person.person_id from Person where Person.name=12345 limit 1) ;"
+                + "SET @destination :=(select Person.person_id from Person where Person.name=12345 limit 1) ;"
+                + "INSERT INTO Person_Person (Person_Person.origin,Person_Person.destination,Person_Person.label ) VALUES (@origin,@destination,@label);", translation);
+    }
+    
+    @Test
+    public void testCreate31() throws FileNotFoundException, IOException {
+        ANTLRInputStream input = new ANTLRInputStream("create (h:Person{name:12345})-[:OWN]->(m:Pet{age:12})");
+        Neo4JLexer lexer = new Neo4JLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Neo4JParser parser = new Neo4JParser(tokens);
+        ParseTree tree = parser.init();
+        MyNeo4JVisitor<Object> loader = new MyNeo4JVisitor<>();
+        String translation = (String) loader.visit(tree);
+        
+        assertEquals("CREATE TABLE IF NOT EXISTS Person_Person (person_person_id LONG NOT NULL AUTO_INCREMENT,destination LONG, FOREIGN KEY (destination) REFERENCES Person(person_id),label LONG, FOREIGN KEY (label) REFERENCES Label(label_id),origin LONG, FOREIGN KEY (origin) REFERENCES Person(person_id), PRIMARY KEY (person_person_id));CREATE TABLE IF NOT EXISTS Label (label_id LONG AUTOINCREMENT, label_name VARCHAR(255) UNIQUE, PRIMARY KEY (label_id));"
+                + "SET @label := (select Label.label_id from Label where Label.label_name=\"EMPLOYEE\" limit 1);"
+                + "SET @origin :=(select Person.person_id from Person where Person.name=12345 limit 1) ;"
+                + "SET @destination :=(select Person.person_id from Person where Person.name=12345 limit 1) ;"
                 + "INSERT INTO Person_Person (Person_Person.origin,Person_Person.destination,Person_Person.label ) VALUES (@origin,@destination,@label);", translation);
     }
 }
